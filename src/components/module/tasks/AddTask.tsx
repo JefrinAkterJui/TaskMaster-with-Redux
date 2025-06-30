2ca5bd3a7cb37312965ddfe322dc2f3f1191e41f
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-06-30 11:25:04 +0600

    addtask complete

---
+++ src/components/module/tasks/AddTask.tsx
@@ -0,0 +1,135 @@
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Textarea } from "@/components/ui/textarea"
+import { cn } from "@/lib/utils"
+import { DialogDescription } from "@radix-ui/react-dialog"
+import { format } from "date-fns"
+import { CalendarIcon } from "lucide-react"
+import { useForm } from "react-hook-form"
+import { Link } from "react-router"
+
+export function AddTaskModal() {
+    const form = useForm()
+
+    const onSubmit =(data: any)=>{
+        console.log(data)
+    }
+
+  return (
+    <Dialog>
+        <DialogTrigger asChild>
+            <Button className=" cursor-pointer">Add Tasks</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+                <DialogTitle>Add Task</DialogTitle>
+            </DialogHeader>
+            <DialogDescription className=" sr-only">this is dialog </DialogDescription>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({field}) => (
+                            <FormItem>
+                            <FormLabel>Title</FormLabel>
+                            <FormControl>
+                                <Input {...field} value={field.value || ""}/>
+                            </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="discription"
+                        render={({field}) => (
+                            <FormItem>
+                            <FormLabel className="mt-4">Discription</FormLabel>
+                            <FormControl>
+                                <Textarea {...field} value={field.value || ""}/>
+                            </FormControl>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="priority"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="mt-4">Priority</FormLabel>
+                                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Select Task Priority" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        <SelectItem value="high">High</SelectItem>
+                                        <SelectItem value="medium">Medium</SelectItem>
+                                        <SelectItem value="low">Low</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="dueDate"
+                        render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                            <FormLabel className="mt-4">Due Date</FormLabel>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <FormControl>
+                                        <Button
+                                            variant={"outline"}
+                                            className={cn(
+                                                " pl-3 text-left font-normal",
+                                                !field.value && "text-muted-foreground"
+                                            )}
+                                        >
+                                            {field.value ? (
+                                            format(field.value, "PPP")
+                                            ) : (
+                                            <span>Pick a date</span>
+                                            )}
+                                            <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                        </Button>
+                                    </FormControl>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-auto p-0" align="start">
+                                    <Calendar
+                                        mode="single"
+                                        selected={field.value}
+                                        onSelect={field.onChange}
+                                        // disabled={(date) =>
+                                        //   date > new Date() || date < new Date("1900-01-01")
+                                        // }
+                                        captionLayout="dropdown"
+                                    />
+                                </PopoverContent>
+                            </Popover>
+                        </FormItem>
+                        )}
+                     />
+                    <DialogFooter className="mt-5">
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </form>
+            </Form>
+        </DialogContent>
+    </Dialog>
+  )
+}
--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -1,3 +1,4 @@
+import { AddTaskModal } from "@/components/module/tasks/AddTask"
 import TaskCard from "@/components/module/tasks/TaskCard"
 import { selecetFilter, selecetTask } from "@/redux/features/task/taskSlice"
 import { useAppSelector } from "@/redux/hooks"
@@ -6,19 +7,20 @@ export default function Task() {
   const tasks= useAppSelector(selecetTask)
   const filter= useAppSelector(selecetFilter)
 
-  console.log(tasks)
-  console.log(filter)
+  // console.log(tasks)
+  // console.log(filter)
 
   return (
     <>
       <div className="max-w-6xl mx-auto mt-20 px-5">
-        <div>
+        <div className=" flex justify-between items-center">
           <h1>Tasks</h1>
+          <AddTaskModal/>
         </div>
         <div className=" space-y-5 mt-5">
           {
             tasks.map((task)=>(
-              <TaskCard task={task}/>
+              <TaskCard task={task} key={task.id}/>
             ))
           }
         </div>
